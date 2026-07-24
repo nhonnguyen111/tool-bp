500f3f741f4e5518f3566b6318d658126108ac57
Nguyễn Văn Nhơn <99854998+nhonnguyen111@users.noreply.github.com>
2026-07-24 08:54:41 +0700

    Replace HashRouter with BrowserRouter

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter,HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
@@ -8,10 +8,10 @@ import AuthProvider from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 );
